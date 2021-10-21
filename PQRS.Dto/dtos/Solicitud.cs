0bf7372275b928b3e858cbf930d26f33ff2e1bfd
Mateo Renteria Lujan <89092316+Mthew1@users.noreply.github.com>
2021-10-20 20:00:30 -0500

    first commit

---
+++ PQRS.Dto/dtos/Solicitud.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PQRS.Dto
+{
+    public abstract class Solicitud
+    {
+        private int area;
+
+        public int Area
+        {
+            get { return area; }
+            set { area = value; }
+        }
+
+        private int idCliente;
+
+        public int IdCliente
+        {
+            get { return idCliente; }
+            set { idCliente = value; }
+        }
+
+
+    }
+}
---
+++ PQRS.Dto/services/SolicitudServices.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PQRS.Dto
+{
+    public class SolicitudServices
+    {
+    }
+}
